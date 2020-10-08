673cefc4dc485b8070f6a3877db8e70d14973751
Riri0000 <ririko010102@gmail.com>
2020-10-08 13:38:20 +0900

    Fix: Factoryの内容にテストケースが依存しないように修正(findで判定するclassを特定)

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -110,11 +110,11 @@ RSpec.describe 'Task', type: :system do
     context '正常系' do
       # FIXME: テストが失敗するので修正してください
       # binding.pryで実行。Task.countは0だから削除はOK。TaskってタイトルをTask was successfully ~で判別している？titleをsequenceにした。
-      it 'Taskが削除されること' do
+      fit 'Taskが削除されること' do
         visit project_tasks_path(project)
         click_link 'Destroy'
         page.driver.browser.switch_to.alert.accept
-        expect(page).not_to have_content task.title
+        expect(find('.task_list')).not_to have_content task.title
         expect(Task.count).to eq 0
         expect(current_path).to eq project_tasks_path(project)
       end
